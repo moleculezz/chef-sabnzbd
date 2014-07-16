ccdee958ce33179953fd94d66e14fccb2f147eca
G. Arends <gd@wmc.io>
2014-07-16 11:49:52 -0400

    update version

--- metadata.rb
+++ metadata.rb
@@ -3,9 +3,9 @@ maintainer       "Thomas Meeus"
 maintainer_email "thomas@sector7g.be"
 license          "MIT"
 description      "Installs and configures sabnzbd"
-version          "1.0.0"
+version          "1.1.0"
 
-supports          "ubuntu", ">= 12.10"
+supports         "ubuntu", ">= 12.10"
 
-depends         "apt"
-depends         "git"
+depends          "apt"
+depends          "git"
